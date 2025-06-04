a815aed8d47bd5a6cabb14961824d88024a381ab
Sandeep992299 <your-email@example.com>
2025-06-04 10:35:12 +0530

    App icon updated.

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,35 @@
-import 'package:crickarena/providers/signup_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:firebase_core/firebase_core.dart'; // NEW
+import 'package:firebase_core/firebase_core.dart';
+
 import 'app.dart';
 import 'providers/cart_provider.dart';
+import 'providers/signup_provider.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized(); // REQUIRED for async main
+  WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      apiKey: "AIzaSyCjkPm6K4swSJ2xaBqBJ39RSlxsOXCrm5A",
-      authDomain: "crickarena-c713c.firebaseapp.com",
-      projectId: "crickarena-c713c",
-      storageBucket: "crickarena-c713c.appspot.com",
-      messagingSenderId: "873824206863",
-      appId: "1:873824206863:android:5c445c427cfd13f6d0e37a",
-      measurementId: "YOUR_MEASUREMENT_ID", // optional
-    ),
-  );
+  // Initialize Firebase only if it hasn't been initialized yet
+  if (Firebase.apps.isEmpty) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyCfawcBAXNx8IpXK1XaJjIZUaQe5zoMU98",
+        authDomain: "crickarena1.firebaseapp.com",
+        projectId: "crickarena1",
+        storageBucket: "crickarena1.appspot.com",
+        messagingSenderId: "399003028616",
+        appId: "1:399003028616:android:98ac7d345fbc0d9d66633d",
+        measurementId: "YOUR_MEASUREMENT_ID", // optional, can be ""
+      ),
+    );
+  }
 
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => CartProvider()),
         ChangeNotifierProvider(create: (_) => SignUpProvider()),
-        // Add other providers like AuthProvider here if needed
+        // Add other providers here
       ],
       child: const CrickArenaApp(),
     ),
